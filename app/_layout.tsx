307a244d213d6edb8dde926009bd212856ef6c49
GYUHWAN JEONG <105405834+solp721@users.noreply.github.com>
2024-12-13 04:12:15 +0900

    feat : 폰트 호출 및 조건부 온보딩 스크린

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,37 @@
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import { Image } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Dimensions, Image } from 'react-native';
+import * as Font from 'expo-font';
+import OnboardingScreen from './screens/onborading/OnboardingScreen';
+
+const { height } = Dimensions.get('window');
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+	const [isOnboarding, setIsOnboarding] = useState(true);
+	const [fontsLoaded, setFontsLoaded] = useState(false);
+
 	useEffect(() => {
-		SplashScreen.hideAsync();
+		const loadFonts = async () => {
+			await Font.loadAsync({
+				NanumSquareRound: require('../assets/fonts/NanumSquareRoundR.ttf'),
+			});
+			setFontsLoaded(true);
+			SplashScreen.hideAsync();
+		};
+
+		loadFonts();
 	}, []);
+
+	const handleOnboardingComplete = () => {
+		setIsOnboarding(false);
+	};
+
+	if (isOnboarding || !fontsLoaded) {
+		return <OnboardingScreen onComplete={handleOnboardingComplete} />;
+	}
 
 	return (
 		<Stack>
@@ -17,13 +40,12 @@ export default function RootLayout() {
 				options={{
 					headerBackTitle: '',
 					headerShown: true,
+
 					header: () => (
 						<Image
 							source={require('../assets/images/header/main-header.png')}
-							style={{
-								width: '100%',
-							}}
 							resizeMode="cover"
+							style={{ width: '100%', height: height * 0.18 }}
 						/>
 					),
 				}}
