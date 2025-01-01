610c8fd8a836a4b90fa2666f14c3e417f8775e86
GYUHWAN JEONG <105405834+solp721@users.noreply.github.com>
2025-01-02 03:27:33 +0900

    feat : eas

--- app.config.js
+++ app.config.js
@@ -1,7 +1,13 @@
 export default {
 	expo: {
+		ios: {
+			bundleIdentifier: 'com.yourapp.identifier',
+		},
 		extra: {
 			apiBaseUrl: process.env.EXPO_PUBLIC_API_BASE_URL,
+			eas: {
+				projectId: 'dd6e189d-c311-4f02-9f6c-53c8daf64979',
+			},
 		},
 	},
 };
