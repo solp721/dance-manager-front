6f86a8cf8e9a3ad1c75f4f065209912e3cb5985d
GYUHWAN JEONG <105405834+solp721@users.noreply.github.com>
2025-01-03 22:37:51 +0900

    feat : expo camera 활용 나의 춤 보기 기능

--- app/(category)/music/detail/my_camera.tsx
+++ app/(category)/music/detail/my_camera.tsx
@@ -1,7 +1,15 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+	View,
+	Text,
+	StyleSheet,
+	ActivityIndicator,
+	Button,
+	ImageBackground,
+} from 'react-native';
 import { Video, ResizeMode } from 'expo-av';
 import { useLocalSearchParams } from 'expo-router';
+import { CameraView, useCameraPermissions } from 'expo-camera';
 
 export default function MyCamera() {
 	const { videoLink } = useLocalSearchParams();
@@ -10,25 +18,76 @@ export default function MyCamera() {
 
 	const [isLoading, setIsLoading] = useState(true);
 
+	const [permission, requestPermission] = useCameraPermissions();
+	const [showCountdown, setShowCountdown] = useState(true);
+	const [countdown, setCountdown] = useState(3);
+	const [countdownText, setCountdownText] = useState('');
+
+	useEffect(() => {
+		if (showCountdown) {
+			const timer = setInterval(() => {
+				setCountdown(prev => {
+					if (prev === 1) {
+						clearInterval(timer);
+						setCountdownText('Start!!');
+						setShowCountdown(false);
+						setTimeout(() => {
+							setCountdownText('');
+						}, 1000);
+						return 0;
+					}
+					return prev - 1;
+				});
+			}, 1000);
+			return () => clearInterval(timer);
+		}
+	}, [showCountdown]);
+
+	if (!permission) {
+		return <View />;
+	}
+
+	if (!permission.granted) {
+		return (
+			<View style={styles.container}>
+				<Text style={styles.message}>
+					나의 춤을 보기위해 카메라 권환이 필요합니다!
+				</Text>
+				<Button onPress={requestPermission} title="grant permission" />
+			</View>
+		);
+	}
+
 	return (
 		<View style={styles.container}>
 			<View style={styles.videoContainer}>
-				<Video
-					source={{ uri: videoUri }}
-					style={styles.video}
-					useNativeControls={false}
-					resizeMode={ResizeMode.COVER}
-					isLooping={true}
-					onLoad={() => setIsLoading(false)}
-				/>
+				{showCountdown || countdownText ? (
+					<ImageBackground source={{ uri: videoUri }} style={styles.video}>
+						<View style={styles.overlay}>
+							<Text style={styles.countdownText}>
+								{countdown > 0 ? countdown : countdownText}
+							</Text>
+						</View>
+					</ImageBackground>
+				) : (
+					<Video
+						source={{ uri: videoUri }}
+						style={styles.video}
+						useNativeControls={false}
+						resizeMode={ResizeMode.COVER}
+						isLooping={true}
+						shouldPlay={true}
+						onLoad={() => setIsLoading(false)}
+					/>
+				)}
 				{isLoading && (
 					<View style={styles.loadingContainer}>
-						<ActivityIndicator size="large" color="#0000ff" />
+						<ActivityIndicator size="large" color="#538BDE" />
 					</View>
 				)}
 			</View>
 			<View style={styles.cameraContainer}>
-				<Text>카메라</Text>
+				<CameraView style={styles.camera} facing={'front'} />
 			</View>
 		</View>
 	);
@@ -38,11 +97,11 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		justifyContent: 'center',
-		alignItems: 'center',
 	},
 	video: {
 		width: '100%',
 		flex: 1,
+		marginTop: -150,
 	},
 	videoContainer: {
 		flex: 1,
@@ -52,9 +111,6 @@ const styles = StyleSheet.create({
 	cameraContainer: {
 		flex: 1,
 		justifyContent: 'center',
-		alignItems: 'center',
-		position: 'relative',
-		width: '100%',
 	},
 	loadingContainer: {
 		position: 'absolute',
@@ -64,6 +120,25 @@ const styles = StyleSheet.create({
 		bottom: 0,
 		justifyContent: 'center',
 		alignItems: 'center',
-		backgroundColor: 'rgba(255, 255, 255, 0.7)',
+	},
+	message: {
+		textAlign: 'center',
+		paddingBottom: 10,
+	},
+	camera: {
+		flex: 1,
+	},
+	overlay: {
+		position: 'absolute',
+		left: 0,
+		right: 0,
+		bottom: 0,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	countdownText: {
+		fontSize: 48,
+		fontFamily: 'NanumSquareRoundB',
+		color: '#538BDE',
 	},
 });
